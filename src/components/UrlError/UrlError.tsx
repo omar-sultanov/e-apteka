ccda98d90f8df55a0ff880983f6ce8500adb86b4
omar.sultanov.98 <omar.sultanov.98@gmail.com>
2022-07-22 10:08:50 +0300

    error page

---
+++ src/components/UrlError/UrlError.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const UrlError = () => {
+  return (
+    <div>
+      <Typography variant="h3">Not Found Page</Typography>
+    </div>
+  );
+};
+
+export default UrlError;
