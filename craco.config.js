a11971ce9ee560276be81e185ac19b6a52819da9
omar.sultanov.98 <omar.sultanov.98@gmail.com>
2022-07-26 10:00:51 +0300

    alias

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, "src/components/"),
+      '@images': path.resolve(__dirname, "src/assets/images/"),
+      '@models': path.resolve(__dirname, "src/models/")
+    }
+  }
+}
+
